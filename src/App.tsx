c04b424b99ba843d37ef9ec16e03e4d48d62be4f
Lionel <massou.lionel@gmail.com>
2022-06-01 18:26:37 +0200

    pokedex_formulaire_part1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PokemonList from "./pages/pokemon-list";
 import PokemonsDetail from "./pages/pokemon-detail";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PageNotFound from "./pages/page-not-found";
+import PokemonEdit from "./pages/pokemon-edit";
 
 const App: FunctionComponent = () => {
 
@@ -19,6 +20,7 @@ const App: FunctionComponent = () => {
           <Switch>
             <Route exact path="/" component={PokemonList} />
             <Route exact path="/pokemons" component={PokemonList} />
+            <Route exact path='/pokemons/edit/:id' component={PokemonEdit} />
             <Route exact path="/pokemons/:id" component={PokemonsDetail} />
             <Route component={PageNotFound} />
           </Switch>
