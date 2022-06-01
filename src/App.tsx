ad3c6158e998a6766a372d471866a3c2f6b2ccf7
Lionel <massou.lionel@gmail.com>
2022-06-01 14:31:13 +0200

    pokedex_first_commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemon-list";
+
+const App: FunctionComponent = () => {
+
+  return (
+      <PokemonList />
+  );
+};
+
+export default App;
