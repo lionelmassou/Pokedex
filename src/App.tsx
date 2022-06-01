59ecd04097447d567c362d7e4362d0409b048210
Lionel <massou.lionel@gmail.com>
2022-06-01 15:30:35 +0200

    pokedex_routes_part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React, { FunctionComponent } from "react";
 import PokemonList from "./pages/pokemon-list";
+import PokemonsDetail from "./pages/pokemon-detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PageNotFound from "./pages/page-not-found";
 
 const App: FunctionComponent = () => {
 
   return (
-      <PokemonList />
+      <Router>
+        <div>
+          {/* la barre de navigation commune à toutes les pages */}
+          <nav>
+            <div className="nav-wrapper teal">
+              <Link to="/" className="brand-logo center">Pokédex</Link>            
+            </div>
+          </nav>
+          {/* le système de gestion des routes de notre application */}
+          <Switch>
+            <Route exact path="/" component={PokemonList} />
+            <Route exact path="/pokemons" component={PokemonList} />
+            <Route exact path="/pokemons/:id" component={PokemonsDetail} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+      </Router>
   );
 };
 
